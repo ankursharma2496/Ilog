5aef885ca7e27fb866d5930c1f5b2d706bee769c
Swati Agarwal <>
2019-04-02 10:03:29 +0530

    initial git setup

---
+++ ILog/ILog.h
@@ -0,0 +1,19 @@
+//
+//  ILog.h
+//  ILog
+//
+//  Created by Ankur Sharma on 02/04/19.
+//  Copyright © 2019 com.ankur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ILog.
+FOUNDATION_EXPORT double ILogVersionNumber;
+
+//! Project version string for ILog.
+FOUNDATION_EXPORT const unsigned char ILogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ILog/PublicHeader.h>
+
+
