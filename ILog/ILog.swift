de5838e909be81ffd11c4b62b951a72a2caf3cbc
Swati Agarwal <>
2019-04-02 10:14:18 +0530

    add log

---
+++ ILog/ILog.swift
@@ -0,0 +1,15 @@
+//
+//  ILog.swift
+//  ILog
+//
+//  Created by Ankur Sharma on 02/04/19.
+//  Copyright © 2019 com.ankur. All rights reserved.
+//
+
+import Foundation
+
+public class ILog{
+  public static func print(str: String){
+    Swift.print(str)
+  }
+}
